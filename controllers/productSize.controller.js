d7e4af521a375cae83bd27de4067a95e7d39e03d
Whitefox23321 <afdhalhadisolahudin@gmail.com>
2026-05-13 11:52:38 +0700

    feat : add update stock

--- controllers/productSize.controller.js
+++ controllers/productSize.controller.js
@@ -22,18 +22,53 @@ module.exports = {
                 size_id: Number(size_id),
                 stock: Number(stock),
             }
-            
+
             const validate = v.validate(data, schema);
             if (validate.length > 0) {
                 return res.status(400).json(response(400, "Validasi error", validate));
             }
             const productSize = await ProductSize.create(data);
 
-            const result =  await ProductSize.findByPk(productSize.id, {include : [Product, Size]});
+            const result = await ProductSize.findByPk(productSize.id, { include: [Product, Size] });
             return res.status(201).json(response(201, "Success", result));
 
         } catch (error) {
             return res.status(500).json(response(500, "Server Error", error.message));
+        }
+    },
+
+    updateProductSize: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { stock } = req.body;
+
+            const schema = {
+                stock: { type: "number", positive: true, integer: true }
+            }
+            const data = {
+                stock: Number(stock)
+            }
+
+            const validate = v.validate(data, schema);
+            if (validate.length > 0) {
+                return res.status(400).json(response(400, "Validasi error", validate));
+            }
+
+            const productSize = await ProductSize.findByPk(id);
+            if(!productSize){
+                return res.status(400).json(response(404, "Product Size [id] not found"));
+            }
+
+            await ProductSize.update(data, {
+                where: {id}
+            });
+
+            const result = await ProductSize.findByPk(productSize.id, { include: [Product, Size] });
+
+            return res.status(200).json(response(200, "Success", result));
+
+        } catch (error) {
+            return res.status(500).json(response(500, "Server Error", error.message))
         }
     }
 }
--- routes/productSize.routes.js
+++ routes/productSize.routes.js
@@ -4,5 +4,6 @@ const upload = require('../middleware/upload')
 const productSizeontroller = require('../controllers/productSize.controller')
 
 router.post('/', upload.none(), productSizeontroller.createProductSize);
+router.put('/:id', upload.none(), productSizeontroller.updateProductSize);
 
 module.exports = router
